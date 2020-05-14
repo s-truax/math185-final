3bb3942e79fd7893f496a9af6f7a61c87a361d91
Sam Truax <samuel.truax@berkeley.edu>
2020-05-13 18:23:11 -0700

    updated matrix scenes, finished except for timings

--- matrix_scenes.py
+++ matrix_scenes.py
@@ -26,3 +26,135 @@ class SimilarityMatrix(Scene):
         self.wait()
 
         self.play(Write(m))
+
+class TotalDerivative(Scene):
+    color_scheme = {}
+    def construct(self):
+        matrix_A = TexMobject("A", "\\in \\text{GL}_2 ( \\mathbf{R} )")
+        f_def = TexMobject("f(x, y) = (u(x,y), v(x,y))")
+        der = TexMobject("""\\begin{pmatrix} u_x & u_y \\\\ v_x & v_y \\end{pmatrix}""")
+        a_alone = TexMobject('A', '=')
+
+        # Here we go, set the colors
+        matrix_A[0].set_color(PURPLE)
+        a_alone[0].set_color(PURPLE)
+
+        f_def[2].set_color(RED) # x
+        f_def[10].set_color(RED)
+        f_def[17].set_color(RED)
+        f_def[4].set_color(BLUE) # y
+        f_def[12].set_color(BLUE)
+        f_def[19].set_color(BLUE)
+        f_def[8].set_color(ORANGE) # u
+        f_def[15].set_color(GREEN) # v
+
+        der[1].set_color(ORANGE) # u
+        der[3].set_color(ORANGE)
+        der[2].set_color(RED) # x
+        der[6].set_color(RED)
+        der[4].set_color(BLUE) # y
+        der[8].set_color(BLUE)
+        der[5].set_color(GREEN) # v
+        der[7].set_color(GREEN)
+
+        matrix_A.move_to(UP + LEFT * 3.5)
+        f_def.move_to(UP + RIGHT * 2)
+        a_alone.move_to(LEFT*2 + DOWN)
+        der.move_to(DOWN)
+
+        self.play(Write(matrix_A))
+
+        self.wait()
+
+        self.play(Write(f_def))
+
+        self.wait()
+
+        self.play(Write(der), Write(a_alone))
+
+class Finale(Scene):
+    def construct(self):
+        f_def = TexMobject("f(x, y) = (u(x,y), v(x,y))")
+        der = TexMobject("""\\begin{pmatrix} u_x & u_y \\\\ v_x & v_y \\end{pmatrix}""")
+        # complex_diff = TextMobject("$f$ complex differentiable")
+        iff1 = TexMobject("\\Leftrightarrow")
+        eq_sign = TexMobject("=")
+        cr = TexMobject("u", "_x", '=', 'v', '_y', "\\, \\, \\text{and} \\, \\,", "u", '_y', '=', '-', 'v', '_x')
+        sim_matrix = TexMobject("""\\begin{pmatrix} \\alpha & -\\beta \\\\
+        \\beta & \\alpha \\end{pmatrix} """)
+        f_complex = TexMobject("""f(x+iy) = u(x, y) + iv(x,y)""")
+
+
+        # Set colors
+        f_def[2].set_color(RED) # x
+        f_def[10].set_color(RED)
+        f_def[17].set_color(RED)
+        f_def[4].set_color(BLUE) # y
+        f_def[12].set_color(BLUE)
+        f_def[19].set_color(BLUE)
+        f_def[8].set_color(ORANGE) # u
+        f_def[15].set_color(GREEN) # v
+
+        f_complex[2].set_color(RED) # x
+        f_complex[10].set_color(RED)
+        f_complex[18].set_color(RED)
+        f_complex[5].set_color(BLUE) # y
+        f_complex[12].set_color(BLUE)
+        f_complex[20].set_color(BLUE)
+        f_complex[8].set_color(ORANGE) # u
+        f_complex[16].set_color(GREEN) # v
+
+        der[1].set_color(ORANGE) # u
+        der[3].set_color(ORANGE)
+        der[2].set_color(RED) # x
+        der[6].set_color(RED)
+        der[4].set_color(BLUE) # y
+        der[8].set_color(BLUE)
+        der[5].set_color(GREEN) # v
+        der[7].set_color(GREEN)
+
+        cr[0].set_color(ORANGE) # u
+        cr[6].set_color(ORANGE)
+        cr[1].set_color(RED) # x
+        cr[11].set_color(RED)
+        cr[3].set_color(GREEN) # v
+        cr[10].set_color(GREEN)
+        cr[4].set_color(BLUE) # y
+        cr[7].set_color(BLUE)
+
+        sim_matrix[1].set_color(ORANGE)
+        sim_matrix[5].set_color(ORANGE)
+        sim_matrix[4].set_color(GREEN)
+        sim_matrix[3].set_color(GREEN)
+
+        f_def.move_to(UP*3)
+        der.move_to(LEFT*5)
+        sim_matrix.move_to(LEFT*2)
+        cr.move_to(RIGHT*3)
+        eq_sign.move_to(LEFT*3.5)
+        iff1.move_to(LEFT*0.2)
+        f_complex.move_to(UP*2 + LEFT*0.18)
+
+        # Brace
+        brace = Brace(der, UP, buff=SMALL_BUFF)
+        b_text = brace.get_text("Real derivative of $f$").scale(0.7)
+
+        brace2 = Brace(sim_matrix, DOWN, buff=SMALL_BUFF)
+        b2_text = brace2.get_text("Form of a rotation and dilation matrix").scale(0.7)
+
+        self.play(Write(f_def))
+        self.play(*[FadeInFrom(f_complex[i], DOWN) for i in range(7, 22)])
+
+        self.play(Write(der))
+        self.play(GrowFromCenter(brace), FadeIn(b_text) )
+        self.play(GrowFromCenter(eq_sign))
+        self.play(Write(sim_matrix))
+        self.play(GrowFromCenter(brace2), FadeIn(b2_text))
+
+        self.wait()
+
+        self.play(GrowFromCenter(iff1))
+
+        self.wait()
+
+        self.play(Write(cr))
